7ad4928f3efd1db1d8a58465189016243b92828e
unknown <ybandar1902@gmail.com>
2019-01-15 22:58:56 +0000

    Testing Random Feature Selection

--- src/random_forest_parts.m
+++ src/random_forest_parts.m
@@ -5,9 +5,22 @@ test_labels = categorical(data{:,'species'});
 test_examples = data;
 test_examples(:,'species') = [];
 
+%=======================================BAGGING================================
 % Fraction of input data to sample with replacement from the input data for growing each new tree
 InBagFraction = 0.1;
 
 % Data to be used to grow the tree
 InBagData = randi(size(test_examples,1),1,size(test_examples,1) * InBagFraction);
 InBagData = test_examples(InBagData, :)
+
+%=======================================FEATURE SELECTION=======================
+% Number of Features to select at random for each decision split.
+numFeatures = 2;
+
+InBagDataColumnSize = size(InBagData,2);
+
+if(numFeatures > InBagDataColumnSize) numFeatures = InBagDataColumnSize; end
+
+% Features to be used
+selectedFeatures = randperm(InBagDataColumnSize,numFeatures);
+selectedFeatures = InBagData(:,selectedFeatures)

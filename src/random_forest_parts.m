b985030adaeb48910bc209f9d45e7b628f86229e
unknown <ybandar1902@gmail.com>
2019-01-15 22:56:30 +0000

    Testing Bagging

--- src/random_forest_parts.m
+++ src/random_forest_parts.m
@@ -3,4 +3,11 @@ data = readtable('iris.csv');
 % format table for testing (in this example whole table is used for testing)
 test_labels = categorical(data{:,'species'});
 test_examples = data;
-test_examples(:,'species') = []
+test_examples(:,'species') = [];
+
+% Fraction of input data to sample with replacement from the input data for growing each new tree
+InBagFraction = 0.1;
+
+% Data to be used to grow the tree
+InBagData = randi(size(test_examples,1),1,size(test_examples,1) * InBagFraction);
+InBagData = test_examples(InBagData, :)

cfd994281387293fe6d5a01809b1e361456e19a3
Yusof Bandar <ybandar1902@gmail.com>
2019-01-18 12:43:07 +0000

    Added Random Forest Predict For One Tree

--- src/myrandomforest.m
+++ src/myrandomforest.m
@@ -30,9 +30,33 @@ classdef myrandomforest
             
             for i=1:num_trees
                 [bag_examples,bag_labels] = myrandomforest.bagging(m);
-                tree = mytree.fit(bag_examples,bag_labels);
-                m.trees{1,i} = tree;
+                tree = mytree.fit(bag_examples,bag_labels,num_features_to_sample);
+                m.trees{end + 1} = tree;
             end
+            
+        end
+        
+        function predictions = predict(m, test_examples)
+            
+            predictions = categorical;
+            
+            for i=1:size(test_examples,1)
+                
+                fprintf('classifying example %i/%i\n', i, size(test_examples,1));
+                this_test_example = test_examples{i,:};
+                
+                tree = m.trees{1,1}
+                
+                this_prediction = mytree.predict_one(tree, this_test_example);
+                predictions(end+1) = this_prediction;
+                
+            end
+        end
+        
+         function prediction = predict_one(m, this_test_example)
+            
+            node = mytree.descend_tree(m.tree, this_test_example);
+            prediction = node.prediction;
             
         end
         
